4ccda2dbe9db364a0ac2b19ce7500602c15e120d
Dipress <budnikovdo@gmail.com>
2017-09-09 11:23:32 +0300

    added pty and user options

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,9 @@ set :rvm_roles, [:app, :web]
 set :repo_url, "git@github.com:Supro/crmifc_frequency.git"
 set :branch, 'master'
 
+set :user, 'deploy'
+set :pty, true
+
 set :rails_env, 'production'
 set :migration_role, :db
 set :migration_servers, -> { primary(fetch(:migration_role)) }
