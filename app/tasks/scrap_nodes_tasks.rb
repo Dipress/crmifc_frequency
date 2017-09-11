52829e453e648f372076db530c0a407181ff633c
Dipress <budnikovdo@gmail.com>
2017-09-11 16:58:28 +0300

    Catch up connection exeption

--- app/tasks/scrap_nodes_tasks.rb
+++ app/tasks/scrap_nodes_tasks.rb
@@ -8,24 +8,28 @@ class ScrapNodesTasks
     nodes = BaseStation.all.map { |node| node.ip }.to_a.compact.sort
 
     nodes.each do |n|
-      login_page = mechanize.get "http://#{n}:85"
-      form = login_page.forms.first
-      form.field_with(id: 'username').value = ENV["bs_login"]
-      form.field_with(id: 'password').value = ENV["bs_password"]
-      form.submit
+      begin
+        login_page = mechanize.get "http://#{n}:85"
+        form = login_page.forms.first
+        form.field_with(id: 'username').value = ENV["bs_login"]
+        form.field_with(id: 'password').value = ENV["bs_password"]
+        form.submit
       
-      data_hash = Hash.new
-      data_hash = mechanize.get "http://#{n}:85/status.cgi"
+        data_hash = Hash.new
+        data_hash = mechanize.get "http://#{n}:85/status.cgi"
       
-      station = BaseStation.where(ip: n)
+        station = BaseStation.where(ip: n)
 
-      actual_frequency = ActualFrequency.where(name: JSON.parse(data_hash.body)["wireless"]["frequency"].gsub(/\s\w{3}/, '')).map {|m| m.id }.first
+        actual_frequency = ActualFrequency.where(name: JSON.parse(data_hash.body)["wireless"]["frequency"].gsub(/\s\w{3}/, '')).map {|m| m.id }.first
 
-      station.update(
-        ssid: JSON.parse(data_hash.body)["host"]["hostname"],
-        mac: JSON.parse(data_hash.body)["interfaces"][2]["hwaddr"],
-        actual_frequency_id: actual_frequency
-        )
+        station.update(
+          ssid: JSON.parse(data_hash.body)["host"]["hostname"],
+          mac: JSON.parse(data_hash.body)["interfaces"][2]["hwaddr"],
+          actual_frequency_id: actual_frequency
+          )
+      rescue Errno::EHOSTUNREACH => e
+        puts "#{e.message}"
+      end
     end
     mechanize.shutdown
     sleep(10)
