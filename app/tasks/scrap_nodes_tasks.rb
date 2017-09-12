62365f8c9d34e36a645868de2004aebd267105f2
Dipress <budnikovdo@gmail.com>
2017-09-12 13:00:14 +0300

    Added platform into script

--- app/tasks/scrap_nodes_tasks.rb
+++ app/tasks/scrap_nodes_tasks.rb
@@ -1,5 +1,6 @@
 require 'mechanize'
 require 'json'
+require 'net/ssh'
 
 class ScrapNodesTasks
   
@@ -16,17 +17,26 @@ class ScrapNodesTasks
         form.submit
       
         data_hash = Hash.new
+        platform = ''
         data_hash = mechanize.get "http://#{n}:85/status.cgi"
+
+        Net::SSH.start("#{n}", "control", password: "adm17in") do |ssh|
+          platform  = ssh.exec!("mca-status | tr ',' '\n'")
+          platform = platform.scan(/^platform=(.*)/) 
+          ssh.close
+        end
       
         station = BaseStation.where(ip: n)
         actual_frequency = ActualFrequency.where(name: JSON.parse(data_hash.body)["wireless"]["frequency"].gsub(/\s\w{3}/, '')).map {|m| m.id }.first
         bandwidth = Bandwidth.where(name: JSON.parse(data_hash.body)["wireless"]["chwidth"]).map {|m| m.id }.first
+        hardware = Hardware.where(name: platform.first.join("")).map {|m| m.id }.first
 
         station.update(
           ssid: JSON.parse(data_hash.body)["wireless"]["essid"],
           mac: JSON.parse(data_hash.body)["interfaces"][2]["hwaddr"],
           actual_frequency_id: actual_frequency,
-          bandwidth_id: bandwidth
+          bandwidth_id: bandwidth,
+          hardware_id: hardware
           )
       rescue Errno::EHOSTUNREACH => e
         puts "#{e.message}"
